16e774fdee948b574883accfebd47365ddc515fa
Adrian Labernia <adrianlabernia@Adrians-MacBook-Air.local>
2023-04-21 13:53:59 +0200

    Added some basic navigation. Basic landing page. It moves to task view

--- client/App.js
+++ client/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, TextInput, Button } from 'react-native';
 import { useState, useEffect } from 'react';
 import { useFonts, PressStart2P_400Regular } from '@expo-google-fonts/press-start-2p';
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack"; 
 
-
-const ChoreButton = ({ title }) => {
+function ChoreButton({ title }) {
   return (
     <TouchableOpacity style={styles.button} >
       <Text style={styles.text}>{title}</Text>
@@ -12,11 +13,15 @@ const ChoreButton = ({ title }) => {
   );
 }
 
-const ListOfChores = () => {
+function ListOfChores() {
   const [chores, setChores] = useState([]);
   const [fontsLoaded] = useFonts({
-    PressStart2P_400Regular,
+    PressStart2P_400Regular
   });
+
+  if (!fontsLoaded) {
+    
+  }
 
   useEffect(() => {
     fetch('http://192.168.0.25:3001/chores')
@@ -37,14 +42,109 @@ const ListOfChores = () => {
   )
 }
 
+function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
 
-export default function App() {
+  const handleLogin = () => {
+    navigation.navigate("Welcome");
+  };
+
+  const handleRegister = () => {
+    navigation.navigate("Register");
+  };
+
+  return (
+    <View style={styles.login.container}>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        style={styles.login.input}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry
+        style={styles.login.input}
+      />
+      <Button title="Login" onPress={handleLogin} />
+      <Button title="Register" onPress={handleRegister} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function RegisterScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const navigation = useNavigation();
+
+  const handleRegister = () => {
+    navigation.navigate("Welcome");
+  };
+
+  return (
+    <View >
+      <Text>Register</Text>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry
+      />
+      <TextInput
+        placeholder="First Name"
+        value={firstName}
+        onChangeText={(text) => setFirstName(text)}
+      />
+      <TextInput
+        placeholder="Last Name"
+        value={lastName}
+        onChangeText={(text) => setLastName(text)}
+      />
+      <Button title="Register" onPress={handleRegister} />
+    </View>
+  );
+}
+
+function WelcomeScreen () {
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.text}>EEEEEE</Text>
       <ListOfChores />
-      <StatusBar style="auto" />
     </ScrollView>
+  );
+}
+
+
+export default function App() {
+  const Stack = createStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      </Stack.Navigator>
+      <StatusBar style="light" backgroundColor="blue"/>
+    </NavigationContainer>
+
+    // <ScrollView style={styles.container}>
+    //   <Text style={styles.text}>EEEEEE</Text>
+    //   <ListOfChores />
+    //   <StatusBar style="auto" />
+    // </ScrollView>
   );
 }
 
@@ -76,5 +176,18 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#000',
     textTransform: 'uppercase',
+  },
+  login: {
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    input: {
+      width: '80%',
+      padding: 10,
+      borderWidth: 1,
+      borderRadius: 5,
+    }
   }
 });
