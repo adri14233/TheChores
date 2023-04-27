c3fa730eff852ac9fba5093bbdae2e6b5910cc10
xaviervide <xaviervidef@gmail.com>
2023-04-27 14:43:22 +0100

    feat: added LoginScreen.js

--- client/App.js
+++ client/App.js
@@ -80,73 +80,73 @@ function ChoreButton({ title, value = 0 }) {
   );
 }
 
-function LoginScreen() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const dispatch = useDispatch();
-  const navigation = useNavigation();
+// function LoginScreen() {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const dispatch = useDispatch();
+//   const navigation = useNavigation();
 
-  const handleLogin = async () => {
-    try {
-      const creds = {
-        username: email,
-        password: password,
-      };
+//   const handleLogin = async () => {
+//     try {
+//       const creds = {
+//         username: email,
+//         password: password,
+//       };
 
-      const response = await fetch("http://192.168.0.25:3001/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(creds),
-      });
+//       const response = await fetch("http://192.168.0.25:3001/login", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(creds),
+//       });
 
-      if (!response.ok) {
-        throw new Error("Failed to get token");
-      }
+//       if (!response.ok) {
+//         throw new Error("Failed to get token");
+//       }
 
-      const data = await response.json();
-      const token = data.token;
+//       const data = await response.json();
+//       const token = data.token;
 
-      dispatch({ type: "SET_EMAIL", payload: email });
-      dispatch({ type: "SET_PASSWORD", payload: password });
-      dispatch({ type: "SET_TOKEN", payload: token });
+//       dispatch({ type: "SET_EMAIL", payload: email });
+//       dispatch({ type: "SET_PASSWORD", payload: password });
+//       dispatch({ type: "SET_TOKEN", payload: token });
 
-      navigation.navigate("Groups");
-    } catch (err) {
-      Alert.alert("Invalid email or password.");
-    }
-  };
+//       navigation.navigate("Groups");
+//     } catch (err) {
+//       Alert.alert("Invalid email or password.");
+//     }
+//   };
 
-  const handleRegister = () => {
-    navigation.navigate("Register");
-  };
+//   const handleRegister = () => {
+//     navigation.navigate("Register");
+//   };
 
-  return (
-    <View style={styles.login.container}>
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-        style={styles.login.input}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        secureTextEntry
-        style={styles.login.input}
-      />
-      <TouchableOpacity style={styles.login.button} onPress={handleLogin}>
-        <Text style={styles.login.buttonText}>LOGIN</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.login.button} onPress={handleRegister}>
-        <Text style={styles.login.buttonText}>REGISTER</Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+//   return (
+//     <View style={styles.login.container}>
+//       <TextInput
+//         placeholder="Email"
+//         value={email}
+//         onChangeText={(text) => setEmail(text)}
+//         style={styles.login.input}
+//       />
+//       <TextInput
+//         placeholder="Password"
+//         value={password}
+//         onChangeText={(text) => setPassword(text)}
+//         secureTextEntry
+//         style={styles.login.input}
+//       />
+//       <TouchableOpacity style={styles.login.button} onPress={handleLogin}>
+//         <Text style={styles.login.buttonText}>LOGIN</Text>
+//       </TouchableOpacity>
+//       <TouchableOpacity style={styles.login.button} onPress={handleRegister}>
+//         <Text style={styles.login.buttonText}>REGISTER</Text>
+//       </TouchableOpacity>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
 // function RegisterScreen() {
 //   const [email, setEmail] = useState("");
---
+++ client/components/LoginScreen.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useDispatch } from "react-redux";
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  const handleLogin = async () => {
+    try {
+      const creds = {
+        username: email,
+        password: password,
+      };
+
+      const response = await fetch("http://192.168.0.25:3001/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(creds),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to get token");
+      }
+
+      const data = await response.json();
+      const token = data.token;
+
+      dispatch({ type: "SET_EMAIL", payload: email });
+      dispatch({ type: "SET_PASSWORD", payload: password });
+      dispatch({ type: "SET_TOKEN", payload: token });
+
+      navigation.navigate("Groups");
+    } catch (err) {
+      Alert.alert("Invalid email or password.");
+    }
+  };
+
+  const handleRegister = () => {
+    navigation.navigate("Register");
+  };
+
+  return (
+    <View style={styles.login.container}>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        style={styles.login.input}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry
+        style={styles.login.input}
+      />
+      <TouchableOpacity style={styles.login.button} onPress={handleLogin}>
+        <Text style={styles.login.buttonText}>LOGIN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.login.button} onPress={handleRegister}>
+        <Text style={styles.login.buttonText}>REGISTER</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
