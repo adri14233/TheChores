1d4b13dae94f834d489d69d1cbe5a1e069f78e40
Maggie Recio <m.reciogrom@gmail.com>
2023-04-29 14:44:30 +0100

    to finish

--- client/App.js
+++ client/App.js
@@ -66,43 +66,43 @@ function ChoreButton({ title, value=0 }) {
 }
 
 
-function TasksScreen () {
-  const [chores, setChores] = useState([]);
-  const isFocused = useIsFocused();
-  const token = useSelector(state => state.token);
+// function TasksScreen () {
+//   const [chores, setChores] = useState([]);
+//   const isFocused = useIsFocused();
+//   const token = useSelector(state => state.token);
 
-  useEffect(() => {
-    getChores('http://192.168.0.25:3001/chores', token).then(choreList => setChores(choreList));
-  }, [isFocused, token]);
+//   useEffect(() => {
+//     getChores('http://192.168.0.25:3001/chores', token).then(choreList => setChores(choreList));
+//   }, [isFocused, token]);
 
-  async function getChores (url, token) {
-    try {
-      const response = await fetch(url, {
-        headers: {
-          "Authorization": `Bearer ${token}`
-      }});
+//   async function getChores (url, token) {
+//     try {
+//       const response = await fetch(url, {
+//         headers: {
+//           "Authorization": `Bearer ${token}`
+//       }});
 
-      if (!response.ok) {
-        throw new Error('Failed to get chores');
-      }
+//       if (!response.ok) {
+//         throw new Error('Failed to get chores');
+//       }
 
-      let chores = await response.json();
-      chores = JSON.parse(chores.message)
-      return chores;
-    } catch (err) {
-      throw new Error(err);
-    }
-  }
+//       let chores = await response.json();
+//       chores = JSON.parse(chores.message)
+//       return chores;
+//     } catch (err) {
+//       throw new Error(err);
+//     }
+//   }
 
-  return (
-    <ScrollView style={styles.container}>
-      <View id="chores-list" style={styles.choresList}>
-      {chores.map((chore, index) => (
-        <ChoreButton key={index} title={chore.name} value={chore.value}/>
-      ))}
-      </View>
-    </ScrollView>
-  );
+//   return (
+//     <ScrollView style={styles.container}>
+//       <View id="chores-list" style={styles.choresList}>
+//       {chores.map((chore, index) => (
+//         <ChoreButton key={index} title={chore.name} value={chore.value}/>
+//       ))}
+//       </View>
+//     </ScrollView>
+//   );
 }
 
 // function NewTaskScreen () {
