39626ef0d0ee036ed02ec644692bc048443a26a8
Maggie Recio <m.reciogrom@gmail.com>
2023-04-29 15:07:51 +0100

    task screen and chore button done

--- client/App.js
+++ client/App.js
@@ -19,51 +19,52 @@ import NewTaskScreen from './components/NewTaskScreen'
 import GroupsScreen from './components/GroupsScreen'
 import LeaderboardScreen from './components/LeaderboardScreen'
 import JoinGroupScreen from './components/JoinGroupScreen'
+import TasksScreen from './components/TasksScreen';
 
 
 
-function ChoreButton({ title, value=0 }) {
-  const token = useSelector(state => state.token);
-  const group = useSelector(state => state.group);
+// function ChoreButton({ title, value=0 }) {
+//   const token = useSelector(state => state.token);
+//   const group = useSelector(state => state.group);
 
-  async function handlePress () {
-    const action = {
-      time: new Date().toString(),
-      user: token,
-      group: group._id,
-      chore: title,
-      value: value
-    }
+//   async function handlePress () {
+//     const action = {
+//       time: new Date().toString(),
+//       user: token,
+//       group: group._id,
+//       chore: title,
+//       value: value
+//     }
 
-    // Add new action to MongoDB
-    try {
-      const resp = await fetch('http://192.168.0.25:3001/action', {
-        method: 'POST',
-        headers: {
-          "Authorization": `Bearer ${token}`,
-          "Content-Type":"application/json"
-        },
-        body: JSON.stringify(action)
-      });
+//     // Add new action to MongoDB
+//     try {
+//       const resp = await fetch('http://192.168.0.25:3001/action', {
+//         method: 'POST',
+//         headers: {
+//           "Authorization": `Bearer ${token}`,
+//           "Content-Type":"application/json"
+//         },
+//         body: JSON.stringify(action)
+//       });
 
-      const data = await resp.json();
-      if (data.message.includes('Action succesfully saved')) Alert.alert("Chore succesfully added!");
-    } catch(err) {
-      Alert.alert("Error", err.message);
-    }
-  }
+//       const data = await resp.json();
+//       if (data.message.includes('Action succesfully saved')) Alert.alert("Chore succesfully added!");
+//     } catch(err) {
+//       Alert.alert("Error", err.message);
+//     }
+//   }
 
-  return (
-    <TouchableOpacity style={styles.choreButton2} onPress={handlePress}>
-        <Text style={styles.text}>{title}</Text>
-        <Image
-          source={{uri: 'https://images.assetsdelivery.com/compings_v2/bldekok/bldekok2108/bldekok210800013.jpg'}}
-          style={{ width: 20, height: 20, marginLeft: 10 }}
-        />
-        <Text style={{fontFamily: 'PressStart2P_400Regular'}}>{value}</Text>
-    </TouchableOpacity>
-  );
-}
+//   return (
+//     <TouchableOpacity style={styles.choreButton2} onPress={handlePress}>
+//         <Text style={styles.text}>{title}</Text>
+//         <Image
+//           source={{uri: 'https://images.assetsdelivery.com/compings_v2/bldekok/bldekok2108/bldekok210800013.jpg'}}
+//           style={{ width: 20, height: 20, marginLeft: 10 }}
+//         />
+//         <Text style={{fontFamily: 'PressStart2P_400Regular'}}>{value}</Text>
+//     </TouchableOpacity>
+//   );
+// }
 
 
 // function TasksScreen () {
@@ -103,7 +104,7 @@ function ChoreButton({ title, value=0 }) {
 //       </View>
 //     </ScrollView>
 //   );
-}
+// }
 
 // function NewTaskScreen () {
 //   const [taskName, setTaskName] = useState("");
